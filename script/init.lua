cfe80bb50d81492f45aba705137ba17795358685
AzureSky <AzureSky.xjc@gmail.com>
2012-07-06 19:15:45 +0800

    update

--- script/init.lua
+++ script/init.lua
@@ -2,22 +2,43 @@ function handler_nihao(args)
 	return true, "zhejiushiniyaodenihao"
 end
 
+function dump_base(args)
+	http.dump()
+	return true, "dump ok"
+end
+
+function show_args(args)
+
+	local a = {123,3245, 2134123, 24134123, "sfasdfdasf", nil,{12314}}
+	local b = {123,3245, 2134123, ["adsfsda"] = 24134123, "sfasdfdasf", {12314}}
+
+	print("a ".. tostring(http.is_array(a)));
+	print("b ".. tostring(http.is_array(b)));
+
+	return true, http.encode_header(args);
+end
+
 local fun_table = {
-	["/nihao"] = handler_nihao
+	["/nihao"] = handler_nihao,
+	["/dump"] = dump_base,
+	["/show"] = show_args,
 }
 
 cnt = 1
 function http_request_handler(args)
-	return true, "buduiba"
+	--return true, "buduiba"
 	local host = args.host
 	local code = args.code
-	local uri = args.uri
+	local path = args.path
 
-	print(string.format("get request(%d) uri(%s) from %s", cnt, uri, host))
+	print(string.format("get request(%d) path(%s) from %s", cnt, path, host))
+	print("query:" ..tostring(args.query));
 	cnt = cnt + 1
 
+	http.get("220.181.112.143", 80 ,"/")
+
 	for k, v in pairs(fun_table) do
-		if uri:find(k) then
+		if path:find(k) then
 			return v(args)
 		end
 	end
