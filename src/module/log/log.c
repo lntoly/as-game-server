46430bac5fbc44aef4c40137ca6f33aef03927c8
AzureSky <AzureSky.xjc@gmail.com>
2012-07-07 14:38:22 +0800

    update log

--- src/module/log/log.c
+++ src/module/log/log.c
@@ -25,21 +25,33 @@ static void make_datetime(struct evbuffer* buf)
 	evbuffer_add_printf(buf, "[%s] ", _make_datetime());
 }
 
-static void _print(struct evbuffer* buf)
-{
-	unsigned char* file_buf = evbuffer_pullup(buf, -1);
-	printf((char*)file_buf);
-}
-
 void log_print(const char* fmt, ...)
 {
+	printf("[%s] ", _make_datetime());
 	va_list args;
 	va_start(args, fmt);
 	vprintf(fmt, args);
 	va_end(args);
+	printf("\n");
 }
 
 void log_file(const char* file, const char* fmt, ...)
+{
+	struct evbuffer* buf = evbuffer_new();
+	if (buf == NULL) return;
+
+	va_list ap;
+	va_start(ap, fmt);
+	evbuffer_add_vprintf(buf, fmt, ap);
+	va_end(ap);
+	evbuffer_add_printf(buf, "\n");
+
+	evbuffer_write_to_file(file, buf, "a+");
+
+	evbuffer_free(buf);
+}
+
+void log_file_with_time(const char* file, const char* fmt, ...)
 {
 	struct evbuffer* buf = evbuffer_new();
 	if (buf == NULL) return;
@@ -53,6 +65,5 @@ void log_file(const char* file, const char* fmt, ...)
 
 	evbuffer_write_to_file(file, buf, "a+");
 
-	_print(buf);
 	evbuffer_free(buf);
 }
--- src/module/log/log.h
+++ src/module/log/log.h
@@ -10,7 +10,12 @@
 
 void log_print(const char*, ...);
 void log_file(const char*, const char*, ...);
+void log_file_with_time(const char*, const char*, ...);
 
-#define log_debug(fmt, arg...) log_file("log/debug.log", fmt, ##arg)
+#define log_debug(fmt, arg...) \
+		do { \
+			log_print(fmt, ##arg); \
+			log_file_with_time("log/debug.log", fmt, ##arg); \
+		} while (0)
 
 #endif /* LOG_H_ */
