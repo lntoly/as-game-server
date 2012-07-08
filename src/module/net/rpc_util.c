eafe64271d3937d2ef9ac040900fe3c6743d110e
AzureSky <AzureSky.xjc@gmail.com>
2012-07-08 22:24:07 +0800

    add rpc

---
+++ src/module/net/rpc_util.c
@@ -0,0 +1,30 @@
+#include "rpc_util.h"
+#include "module/vm/util.h"
+
+void rpc_dispatch(struct evbuffer* packet, lua_State* L)
+{
+	if (find_lua_function(L, "rpc", "dispatch")) {
+		int base = lua_gettop(L);
+		//TODO unpack
+
+		int args = lua_gettop(L) - base;
+		if (args < 0) args = 0;
+		call_lua_script(L, args);
+	}
+}
+
+static int lua_rpc_pack(lua_State* L)
+{
+	if (lua_gettop(L) < 2) return 0;
+	return 0;
+}
+
+static const struct luaL_Reg lua_rpc_function[] = {
+		{"rpc_pack", lua_rpc_pack},
+		{NULL, NULL}
+};
+
+void reg_lua_rpc(lua_State* L)
+{
+	luaL_register(L, "_G", lua_rpc_function);
+}
---
+++ src/module/net/rpc_util.h
@@ -0,0 +1,19 @@
+/*
+ * rpc_util.h
+ *
+ *  Created on: 2012-7-8
+ *      Author: AzureSky
+ */
+
+#ifndef RPC_UTIL_H_
+#define RPC_UTIL_H_
+
+#include <event2/buffer.h>
+#include "module/vm/vm.h"
+
+void init_rpc(struct event_base*);
+void rpc_dispatch(struct evbuffer*, lua_State*);
+
+void reg_lua_rpc(lua_State*);
+
+#endif /* RPC_UTIL_H_ */
