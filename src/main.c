e26043de25fd62189ae2cf78d680b3061d8912aa
AzureSky <AzureSky.xjc@gmail.com>
2012-07-08 22:24:49 +0800

    update

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
-#include "os/os.h"
-#include "program/httpd/httpd.h"
+#include "module/os/os.h"
+//#include "program/httpd/httpd.h"
+#include "program/echo/echo.h"
 
 int main(void) {
 	init_os();
 
-	init_httpd();
-	close_httpd();
+	init_echo();
+	close_echo();
 
+	close_os();
 	return 0;
 }
